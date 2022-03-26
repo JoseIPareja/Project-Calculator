948f7747a70bcdb4a734fc9eb2566d5e658dfec3
Jose Ignacio Pareja <jose.ignacio97@gmail.com>
2022-03-26 17:55:33 -0300

    3rd | Script: Added Clear & Delete Interfaces. Created a Prototype for Function Calculate.

--- script.js
+++ script.js
@@ -18,33 +18,60 @@ function divideNumbers(num1, num2) {
     return num1 / num2;
 }
 
-// FUNCTION TO DISPLAY ABOVE FUNCTIONS
-function display(operator, num1, num2){
-    switch(operator){
-        case '+':
-            return addNumbers(num1, num2), console.log(addNumbers(num1, num2)); 
-            break;
-        case '-':
-            return substractNumbers(num1, num2), console.log(addNumbers(num1, num2));
-            break;
-        case '*':
-            return multiplyNumbers(num1, num2), console.log(addNumbers(num1, num2));
-            break;
-        case '/':
-            return divideNumbers(num1, num2), console.log(addNumbers(num1, num2));
-            break;
-    }
-}
-
-// DISPLAY NUMBERS ON SCREEN
-const buttons = document.querySelectorAll('.small');
+// DISPLAY NUMBERS ON SCREEN & CONSOLE
+const numberbuttons = document.querySelectorAll('.small');
 const screen = document.querySelector('.screen');
-buttons.forEach((button) => {
+numberbuttons.forEach((button) => {
     button.addEventListener('click', () => {
         screen.textContent += button.textContent;
+        let digit = button.textContent;
+        console.log(digit);
     })
 })
 
-//const operator = document.querySelector('.operator');
-//const displayer = document.querySelector('.displayer');
-//displayer.addEventListener('click', display(operator.textContent);
+// DISPLAY OPERATORS ON SCREEN & CONSOLE
+const operatebuttons = document.querySelectorAll('.operator');
+operatebuttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        screen.textContent += button.textContent;
+        let operator = button.textContent;
+        console.log(operator);
+    })
+})
+
+// FUNCTION TO CALCULATE WHAT IS ON THE SCREEN
+function calculate() {
+    let contenido = screen.textContent;
+    let contenidoDividido = contenido.split(/(\D)/);
+    if (contenidoDividido[1] === "+") {
+        let resultado = addNumbers(parseFloat(contenidoDividido[0]), parseFloat(contenidoDividido[2])); 
+        return screen.textContent = resultado;
+    } else if (contenidoDividido[1] === "-") {
+        let resultado = substractNumbers(parseFloat(contenidoDividido[0]), parseFloat(contenidoDividido[2])); 
+        return screen.textContent = resultado;
+    } else if (contenidoDividido[1] === "x") {
+        let resultado =multiplyNumbers(parseFloat(contenidoDividido[0]), parseFloat(contenidoDividido[2])); 
+        return screen.textContent = resultado;
+    } else if (contenidoDividido[1] === "/") {
+        let resultado =divideNumbers(parseFloat(contenidoDividido[0]), parseFloat(contenidoDividido[2])); 
+        return screen.textContent = resultado;
+    } else return;
+}
+
+// DISPLAY INTERFACE
+const displayer = document.querySelector('.displayer');
+displayer.addEventListener('click', calculate);
+
+
+// CLEAR INTERFACE
+const clear = document.querySelector('.clear');
+clear.addEventListener('click', () => {
+    screen.textContent = "";
+})
+
+// DELETE INTERFACE
+const del = document.querySelector('.delete');
+del.addEventListener('click', () => {
+    let contenido = screen.textContent;
+    screen.textContent = contenido.substring(0, contenido.length - 1);
+})
